107b045c1608f475cbee00eade3182507069477d
issue87 <issue87@mail.ru>
2024-03-24 22:09:41 +0300

    debugging

--- static_files/js/game_most_js.js
+++ static_files/js/game_most_js.js
@@ -306,7 +306,7 @@ CardValues = {0:2,1:3,2:4,3:5,4:6,5:7,6:8,7:9,8:10,9:10,10:10,11:10,12:11};
             balanceImage.onload = detectLoading;
             sliderButton.onload = detectLoading;
             sliderCashBase.onload = detectLoading;
-
+            detectLoading();
 
 
         };

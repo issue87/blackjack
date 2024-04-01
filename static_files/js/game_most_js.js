a4f4839802330cda5527b6032aebe306c6eabc9c
issue87 <issue87@mail.ru>
2024-04-02 00:00:18 +0300

    debug

--- static_files/js/game_most_js.js
+++ static_files/js/game_most_js.js
@@ -198,8 +198,8 @@ CardValues = {0:2,1:3,2:4,3:5,4:6,5:7,6:8,7:9,8:10,9:10,10:10,11:10,12:11};
                 canvasScaleRatio = canvasWidth/canvasEl.style.width;
                 ctx = canvasEl.getContext("2d");
                 ctx.scale(canvasScaleRatio, canvasScaleRatio);
-                canvas.style.width = `${canvasWidth}px`;
-                canvas.style.height = `${canvasHeight}px`;
+                canvasEl.style.width = `${canvasWidth}px`;
+                canvasEl.style.height = `${canvasHeight}px`;
                 drawCanvasAnimation();
             };
             var resultGameHandler = function (res){

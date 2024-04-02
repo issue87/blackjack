82fcc4107d932dbc7d33623b580c86b2aab979c6
issue87 <issue87@mail.ru>
2024-04-02 23:25:01 +0300

    debug

--- static_files/js/game_most_js.js
+++ static_files/js/game_most_js.js
@@ -199,7 +199,7 @@ CardValues = {0:2,1:3,2:4,3:5,4:6,5:7,6:8,7:9,8:10,9:10,10:10,11:10,12:11};
                 canvasScaleRatio = canvasWidth/canvasEl.style.width;
                 controlElements = document.getElementsByClassName("gameControl");
                 for (let i;i < controlElements.length;i++){
-                    controlElements[i].style.top = controlElements[i].style.top * canvasScaleRatio;
+                    controlElements[i].style.top = parseInt(controlElements[i].style.top) * canvasScaleRatio;
                 }
                 ctx = canvasEl.getContext("2d");
                 ctx.scale(canvasScaleRatio, canvasScaleRatio);

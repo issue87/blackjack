1abf3c396816a5bae3320728999f783f24ef3ff6
issue87 <issue87@mail.ru>
2024-04-04 23:47:01 +0300

    debugging

--- static_files/js/game_most_js.js
+++ static_files/js/game_most_js.js
@@ -198,7 +198,7 @@ CardValues = {0:2,1:3,2:4,3:5,4:6,5:7,6:8,7:9,8:10,9:10,10:10,11:10,12:11};
                     canvasWidth = (window.innerHeight/initialCanvasHeight) * initialCanvasWidth;
                 };
                 console.log(canvasWidth);
-                console.log(canvasHeight);
+                console.log(initialCanvasWidth);
                 canvasScaleRatio = canvasWidth/initialCanvasWidth;
                 controlElements = document.getElementsByClassName("gameControl");
                 const initialGameControlTopPos = 425;

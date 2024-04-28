55fecfb3f38a64917f767ef0fdac8ac4d54faa33
issue87 <issue87@mail.ru>
2024-04-28 23:19:12 +0300

    test

--- static_files/js/game_most_js.js
+++ static_files/js/game_most_js.js
@@ -215,7 +215,7 @@ CardValues = {0:2,1:3,2:4,3:5,4:6,5:7,6:8,7:9,8:10,9:10,10:10,11:10,12:11};
             function dropDown(){
                const items = document.getElementsByClassName("menuItem");
                for (let i = 0; i < items.length;i++){
-                    console.log(element.style.visibility);
+                    console.log(items[i].style.visibility);
                     if (items[i].style.visibility == "hidden"){
                     console.log("worked out");
                     };

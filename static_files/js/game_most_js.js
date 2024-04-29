ad2a8c0115afcf5f41962dc178496654d6298175
issue87 <issue87@mail.ru>
2024-04-29 22:44:13 +0300

    debug

--- static_files/js/game_most_js.js
+++ static_files/js/game_most_js.js
@@ -217,13 +217,14 @@ CardValues = {0:2,1:3,2:4,3:5,4:6,5:7,6:8,7:9,8:10,9:10,10:10,11:10,12:11};
                const items = document.getElementsByClassName("menuItem");
                for (let i = 0; i < items.length;i++){
                     if (droppedDown){
-                        droppedDown = false;
                         items[i].style.visibility = "hidden";
                     }else{
-                        droppedDown = true;
                         items[i].style.visibility = "visible";
                     }
                     ;
+               };
+               if (droppedDown){
+                  droppedDown = false;
                };
             };
             function stand()

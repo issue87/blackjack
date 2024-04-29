04592a9299217ee717b030532187fb6b35df69a8
issue87 <issue87@mail.ru>
2024-04-29 22:47:02 +0300

    debug

--- static_files/js/game_most_js.js
+++ static_files/js/game_most_js.js
@@ -225,6 +225,8 @@ CardValues = {0:2,1:3,2:4,3:5,4:6,5:7,6:8,7:9,8:10,9:10,10:10,11:10,12:11};
                };
                if (droppedDown){
                   droppedDown = false;
+               }else{
+                  droppedDown = true;
                };
             };
             function stand()

912d524787a6990b55a2e1ff8315c3ef2281acf3
issue87 <issue87@mail.ru>
2024-05-08 23:40:08 +0300

    debug

--- static_files/js/game_most_js.js
+++ static_files/js/game_most_js.js
@@ -285,8 +285,7 @@ CardValues = {0:2,1:3,2:4,3:5,4:6,5:7,6:8,7:9,8:10,9:10,10:10,11:10,12:11};
                 console.log(window.visualViewport.scale);
                 console.log(screen.width);
                 console.log(window.innerWidth);
-                if (screen.width == window.innerWidth && window.visualViewport.scale == 1){
-                    console.log("worked");
+                if (screen.width == window.innerWidth && window.innerWidth > 867){
                     canvasWidth = 867;
                     canvasHeight = 493;
                 }

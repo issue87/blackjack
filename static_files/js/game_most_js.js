6ec8e2d1402f1fcf220ed4c48a78d34ae1111253
issue87 <issue87@mail.ru>
2024-05-08 23:46:25 +0300

    debug

--- static_files/js/game_most_js.js
+++ static_files/js/game_most_js.js
@@ -285,9 +285,9 @@ CardValues = {0:2,1:3,2:4,3:5,4:6,5:7,6:8,7:9,8:10,9:10,10:10,11:10,12:11};
                 console.log(window.visualViewport.scale);
                 console.log(screen.width);
                 console.log(window.innerWidth);
-                if (screen.width == window.innerWidth && window.innerWidth > 867){
-                    canvasWidth = 867;
-                    canvasHeight = 493;
+                if (screen.width == window.innerWidth && window.innerWidth > 867 && window.innerHeight > 493){
+                    canvasWidth = 867/window.visualViewport.scale;
+                    canvasHeight = 493/window.visualViewport.scale;
                 }
                 const canvasScaleRatio = canvasWidth/initialCanvasWidth;
                 GameControlGroup.scaleObjects(gameControlObjects, canvasScaleRatio);

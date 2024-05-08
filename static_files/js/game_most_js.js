aa1a817b3409f34d6d75d38117f80252ef9c72c0
issue87 <issue87@mail.ru>
2024-05-09 00:49:58 +0300

    debuging

--- static_files/js/game_most_js.js
+++ static_files/js/game_most_js.js
@@ -300,6 +300,7 @@ CardValues = {0:2,1:3,2:4,3:5,4:6,5:7,6:8,7:9,8:10,9:10,10:10,11:10,12:11};
                 nodes = document.querySelectorAll(".menuItem, .menuButton, #userName");
                 for (let i; i < nodes.length; i++){
                     nodes[i].style.setProperty('--buttonFontSize', `${newFontSize}px`);
+                    console.log(nodes[i]);
                 }
                 ctx = canvasEl.getContext("2d");
                 ctx.scale(canvasScaleRatio, canvasScaleRatio);

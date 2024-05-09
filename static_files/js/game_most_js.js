5784b5ec076f23d799c352bb9d3ed46c6ad29173
issue87 <issue87@mail.ru>
2024-05-09 22:03:10 +0300

    debug

--- static_files/js/game_most_js.js
+++ static_files/js/game_most_js.js
@@ -285,6 +285,7 @@ CardValues = {0:2,1:3,2:4,3:5,4:6,5:7,6:8,7:9,8:10,9:10,10:10,11:10,12:11};
                 if (canvasWidth/devicePixelRatio > 867){
                     canvasWidth = 867 * devicePixelRatio;
                     canvasHeight = 493 * devicePixelRatio;
+                    console.log("limit canvas:>867");
                 }
                 const canvasScaleRatio = canvasWidth/initialCanvasWidth;
                 GameControlGroup.scaleObjects(gameControlObjects, canvasScaleRatio);

86ac1a4ad12bfc4ae19df48d1ed21ed637bf8cd7
issue87 <issue87@mail.ru>
2024-05-09 22:37:39 +0300

    debug

--- static_files/js/game_most_js.js
+++ static_files/js/game_most_js.js
@@ -267,28 +267,20 @@ CardValues = {0:2,1:3,2:4,3:5,4:6,5:7,6:8,7:9,8:10,9:10,10:10,11:10,12:11};
                 let canvasWidth;
                 let canvasHeight;
                 if (screen.availWidth/initialCanvasWidth < screen.availHeight/initialCanvasHeight){
-                    canvasWidth = window.innerWidth;
-                    //canvasWidth = screen.availWidth;
-                    canvasHeight = (window.innerWidth/initialCanvasWidth) * initialCanvasHeight;
-                    //canvasHeight = (screen.availWidth/initialCanvasWidth) * initialCanvasHeight;
+                    //canvasWidth = window.innerWidth;
+                    canvasWidth = screen.availWidth;
+                    //canvasHeight = (window.innerWidth/initialCanvasWidth) * initialCanvasHeight;
+                    canvasHeight = (screen.availWidth/initialCanvasWidth) * initialCanvasHeight;
                 }else{
-                    canvasHeight = window.innerHeight;
-                    //canvasHeight = screen.availHeight;
-                    canvasWidth = (window.innerHeight/initialCanvasHeight) * initialCanvasWidth;
-                    //canvasWidth = (screen.availHeight/initialCanvasHeight) * initialCanvasWidth;
+                    //canvasHeight = window.innerHeight;
+                    canvasHeight = screen.availHeight;
+                    //canvasWidth = (window.innerHeight/initialCanvasHeight) * initialCanvasWidth;
+                    canvasWidth = (screen.availHeight/initialCanvasHeight) * initialCanvasWidth;
                 };
                 const devicePixelRatio = window.devicePixelRatio;
                 //adjusting width and height of the canvas with user device's scale of the page
-                //canvasWidth /= window.visualViewport.scale;
-                //canvasHeight /= window.visualViewport.scale;
-                //limiting maximal size of canvas
-                console.log(canvasWidth);
-                console.log(devicePixelRatio);
-                if (canvasWidth > 867){
-                    canvasWidth = 867;
-                    canvasHeight = 493;
-                    console.log("limit canvas:>867");
-                }
+                canvasWidth /= window.visualViewport.scale;
+                canvasHeight /= window.visualViewport.scale;
                 const canvasScaleRatio = canvasWidth/initialCanvasWidth;
                 GameControlGroup.scaleObjects(gameControlObjects, canvasScaleRatio);
                 const rangeElement = document.getElementById("sliderRange");

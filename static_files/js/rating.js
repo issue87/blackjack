65c3ddc58ac60265ad301e5f3b75558094b17897
issue87 <issue87@mail.ru>
2024-05-20 22:31:17 +0300

    debugging

--- static_files/js/rating.js
+++ static_files/js/rating.js
@@ -1,8 +1,10 @@
 const navLinks = document.getElementsByClassName("ratingNavigationLink");
 const navForm = document.getElementById("ratingNavigationForm");
 const targetPageInput = document.getElementById("targetNavPage");
+console.log("debugging1");
 for (let i = 0; i<navLinks.length; i++){
     navLinks[i].addEventListener("click",submitNavForm);
+    console.log("debugging2");
 };
 function submitNavForm(){
     const incrementStartRow = Number(this.dataset.increment);

df8c8c4ed626ba6532c3665d2eaaf459a8084e9f
issue87 <issue87@mail.ru>
2024-05-26 18:03:09 +0300

    changing anchor links style

--- static_files/js/rating.js
+++ static_files/js/rating.js
@@ -1,11 +1,8 @@
 const navLinks = document.getElementsByClassName("ratingNavigationLink");
 const navForm = document.getElementById("ratingNavigationForm");
 const targetPageInput = document.getElementById("targetNavPage");
-console.log(navLinks);
 iterableNavLinks = Array.from(navLinks);
-console.log(iterableNavLinks);
 for (let node of iterableNavLinks){
-    console.log("innerLoop");
     node.addEventListener("click",submitNavForm);
 };
 function submitNavForm(){

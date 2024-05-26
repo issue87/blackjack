59d5e95dc70cd495756f487f035ff8486827c6e8
issue87 <issue87@mail.ru>
2024-05-26 20:48:47 +0300

    debugging

--- static_files/js/game_most_js.js
+++ static_files/js/game_most_js.js
@@ -421,6 +421,10 @@ CardValues = {0:2,1:3,2:4,3:5,4:6,5:7,6:8,7:9,8:10,9:10,10:10,11:10,12:11};
                  if (images == 6){
                     const gameContainer = document.getElementById("gameContainer");
                     gameContainer.style.visibility = "visible";
+                    const gameControlBet = document.getElementById("gameControlBet");
+                    const gameControlSlider = document.getElementById("gameControlSlider");
+                    gameControlBet.style.visibility = "visible";
+                    gameControlSlider.style.visibility = "visible";
                     resizeCanvas();
                  };
             };

8388e3d332ff8a10193ba3cc6c92176403e28e06
issue87 <issue87@mail.ru>
2024-06-02 16:41:37 +0300

    deleting button bet as redundant

--- static_files/js/game_most_js.js
+++ static_files/js/game_most_js.js
@@ -257,11 +257,6 @@ CardValues = {0:2,1:3,2:4,3:5,4:6,5:7,6:8,7:9,8:10,9:10,10:10,11:10,12:11};
                  };
             };
 
-            function bet() {
-                 betInGame = betInSlider;
-                 drawCanvasAnimation();
-            };
-
             function resizeCanvas(){
                 const canvasEl = document.getElementById('gameCanvas');
                 let canvasWidth;
